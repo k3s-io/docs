41ea334326ca91160a8846b29ad3e1e3350d5f2e
Divya Mohan <divya.mohan0209@gmail.com>
2022-07-06 17:46:53 +0530

    Adding first commit for k3s-docs Signed-off-by: Divya Mohan <divya.mohan0209@gmail.com>
    Signed-off-by: Divya Mohan <divya.mohan0209@gmail.com>

---
+++ docusaurus.config.js
@@ -0,0 +1,70 @@
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'K3s',
+  tagline: '',
+  url: 'https://rancher.com/docs/k3s/latest/en',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.png',
+  organizationName: 'rancher', // Usually your GitHub org/user name.
+  projectName: 'k3s-docs', // Usually your repo name.
+  trailingSlash: false,
+  themeConfig: {
+    colorMode: {
+      // "light" | "dark"
+      defaultMode: "light",
+
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: false,
+    },
+    navbar: {
+      title: "",
+      logo: {
+        alt: 'logo',
+        src: 'img/k3s-horizontal-color.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'introduction',
+          position: 'right',
+          label: 'Docs',
+          className: 'navbar__docs',
+        },
+        {
+          href: 'https://github.com/kubewarden/',
+          label: 'GitHub',
+          position: 'right',
+          className: 'navbar__github btn btn-secondary icon-github',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} SUSE Rancher. All Rights Reserved.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          /* other docs plugin options */
+          sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateTime: true,
+          editUrl: 'https://github.com/kubewarden/docs/edit/main/',
+        },
+        blog: false, // Optional: disable the blog plugin
+        // ...
+        theme: {
+          customCss: [require.resolve("./src/css/custom.css")],
+        },
+      },
+    ],
+  ], 
+};
