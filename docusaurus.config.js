2eb5bc71e8d8a3b44d1bf754f194afd0245c637a
Derek Nola <derek.nola@suse.com>
2023-12-06 09:37:53 -0800

    Remove release notes from search, Docusaurous Bump (#214)
    * Don't index release notes in search
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    * Bump docusaurous to 3.0.1
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    ---------
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ module.exports = {
         hashed: true,
         highlightSearchTermsOnTargetPage: true,
         indexBlog: false,
+        ignoreFiles: [/release-notes\/.*/],
       }),
     ],
   ],
